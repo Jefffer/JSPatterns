c317e451c7adc0e99b2145c6da22a1ae24991798
Jefffer <jefre123@hotmail.com>
2018-05-07 12:33:43 -0500

    Constructor Pattern creation

---
+++ js/Creational Design Patterns/Constructor Pattern/jsConstructorPattern.js
@@ -0,0 +1,10 @@
+// JavaScript - Design Patterns
+// Constructor Pattern
+
+/* 
+"new" keyword:
+in front of a function, it creates a constructor of that function, creates a brand new object,
+links to an object prototype, binds "this" to the new object scope, implicity return this
+*/
+
+var a; 
